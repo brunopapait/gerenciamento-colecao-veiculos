4dcf814cfb6b315ddd3242b431ba5954695d605f
brunopapait <brunohpapait@gmail.com>
2022-08-02 00:02:50 -0300

    add new dependency button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatButtonModule} from '@angular/material/button';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CountVehiclesComponent } from './components/count-vehicles/count-vehicles.component';
+import { ButtonRegisterComponent } from './components/button-register/button-register.component';
+import { SubHeaderComponent } from './components/sub-header/sub-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CountVehiclesComponent,
+    ButtonRegisterComponent,
+    SubHeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]

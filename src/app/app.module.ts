593e0cb2e08111561ad8b0f64ae4968b1dc51132
brunopapait <brunohpapait@gmail.com>
2022-08-11 22:39:45 -0300

    import new components and modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { CountVehiclesComponent } from './components/count-vehicles/count-vehicles.component';
 import { ButtonRegisterComponent } from './components/button-register/button-register.component';
 import { SubHeaderComponent } from './components/sub-header/sub-header.component';
+import { VehiclesComponent } from './pages/vehicles/vehicles.component';
+import { VehiclesFormComponent } from './pages/vehicles-form/vehicles-form.component';
+import { VehiclesListContainerComponent } from './components/vehicles-list-container/vehicles-list-container.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +20,9 @@ import { SubHeaderComponent } from './components/sub-header/sub-header.component
     CountVehiclesComponent,
     ButtonRegisterComponent,
     SubHeaderComponent,
+    VehiclesComponent,
+    VehiclesFormComponent,
+    VehiclesListContainerComponent,
   ],
   imports: [
     BrowserModule,

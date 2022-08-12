4852c95012ad54227b5b4d3bb5832ac40f33f2f5
brunopapait <brunohpapait@gmail.com>
2022-08-11 22:35:25 -0300

    refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { VehiclesFormComponent } from './pages/vehicles-form/vehicles-form.component';
+import { VehiclesComponent } from './pages/vehicles/vehicles.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'vehicles', pathMatch: 'full' },
+  { path: 'vehicles', component: VehiclesComponent},
+  { path: 'vehicles/new', component: VehiclesFormComponent },
+  { path: 'vehicles/edit/:id', component: VehiclesFormComponent }]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

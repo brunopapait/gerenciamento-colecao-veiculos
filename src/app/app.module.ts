d171dd414d1cf1b7f5520eb1ab4b6e151ca6e905
brunopapait <brunohpapait@gmail.com>
2022-08-17 00:04:39 -0300

    feat: add new modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
 
 import {MatButtonModule} from '@angular/material/button';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,8 +30,12 @@ import { VehiclesListContainerComponent } from './components/vehicles-list-conta
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]

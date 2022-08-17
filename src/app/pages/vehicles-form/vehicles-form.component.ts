5e74c7a2170c4433c977a0fb7f70e4b485bb9258
brunopapait <brunohpapait@gmail.com>
2022-08-17 00:05:34 -0300

    feat: add new code to form vehicle

--- src/app/pages/vehicles-form/vehicles-form.component.ts
+++ src/app/pages/vehicles-form/vehicles-form.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Vehicle } from './../../model/Vehicle';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-vehicles-form',
   templateUrl: './vehicles-form.component.html',
@@ -8,13 +9,28 @@ import { ActivatedRoute } from '@angular/router';
 })
 
 export class VehiclesFormComponent implements OnInit {
+  @ViewChild('form') form!: NgForm;
+
+  vehicle: Vehicle = new Vehicle('', '', '', '', '', '', 0, 0, 0, 0, new Date(), new Date());
   idVehicle: number = 0;
   constructor(private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+
     this.activatedRoute.params.subscribe(({id}) => this.idVehicle = id);
     if (!this.idVehicle) return;
 
     alert(this.idVehicle);
+  }
+
+  onSubmit() {
+  }
+
+  getErrorMessage() {
+    // if (this.email.hasError('required')) {
+    //   return 'You must enter a value';
+    // }
+
+    // return this.email.hasError('email') ? 'Not a valid email' : '';
   }
 }

911b84e20a44ccbb8aea08a509c7441ffa1ad041
brunopapait <brunohpapait@gmail.com>
2022-08-23 23:56:08 -0300

    feat: add new atributtes and new method save

--- src/app/services/vehicle.service.ts
+++ src/app/services/vehicle.service.ts
@@ -7,9 +7,25 @@ import { Vehicle } from '../model/Vehicle';
 })
 export class VehicleService {
   URI = 'http://localhost:3000/vehicles';
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
   constructor(private httpClient: HttpClient) { }
 
-  findAll() {
-    return this.httpClient.get(this.URI).toPromise().then(response => response as Vehicle[]);
+  async save(vehicle: Vehicle) {
+    return this.httpClient.post(this.URI, JSON.stringify(vehicle), this.httpOptions).toPromise()
+    .then(response => response as Vehicle)
+    .catch(() => {
+      throw new Error('Erro ao salvar o veículo !');
+    });
+  }
+
+  async findAll() {
+    return this.httpClient.get(this.URI).toPromise()
+    .then(response => response as Vehicle[])
+    .catch(() => {
+      throw new Error('Erro ao buscar o veículo !');
+    });
   }
 }

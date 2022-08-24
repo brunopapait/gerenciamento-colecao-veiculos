7f0b36ab8ea5276917c6876e5563701bc2695e47
brunopapait <brunohpapait@gmail.com>
2022-08-23 23:43:30 -0300

    feat: refactor code

--- src/app/services/vehicle.service.ts
+++ src/app/services/vehicle.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Vehicle } from '../model/Vehicle';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VehicleService {
+  URI = 'http://localhost:3000/vehicles';
+  constructor(private httpClient: HttpClient) { }
 
-  constructor() { }
+  findAll() {
+    return this.httpClient.get(this.URI).toPromise().then(response => response as Vehicle[]);
+  }
 }

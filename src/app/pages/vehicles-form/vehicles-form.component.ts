d1ac44465d52f26e00e0be54a179f12a4eacb909
brunopapait <brunohpapait@gmail.com>
2022-09-02 22:19:07 -0300

    feat: refactor code and create new methods (update)

--- src/app/pages/vehicles-form/vehicles-form.component.ts
+++ src/app/pages/vehicles-form/vehicles-form.component.ts
@@ -1,7 +1,6 @@
 import { Vehicle } from './../../model/Vehicle';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
-import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { DialogComponent } from 'src/app/components/dialog/dialog.component';
@@ -17,37 +16,52 @@ import { VehicleService } from 'src/app/services/vehicle.service';
 export class VehiclesFormComponent implements OnInit {
   @ViewChild('form') form!: NgForm;
 
-  vehicle!: Vehicle;
-  idVehicle: number = 0;
+  vehicle: Vehicle = new Vehicle(this.generatedUniqueId(), 'ativo', '', '', '', '', 0, 0, 2022, 2022);
+  idVehicle: string = '';
   constructor(private activatedRoute: ActivatedRoute, public dialog: MatDialog, private location: Location, private vehicleService: VehicleService) { }
 
   ngOnInit(): void {
-
     this.activatedRoute.params.subscribe(({id}) => this.idVehicle = id);
     if (!this.idVehicle) {
-       this.vehicle = new Vehicle(this.generatedUniqueId(), 'ativo', '', '', '', '', 0, 0, 2022, 2022);
        return;
     };
 
-    alert(this.idVehicle);
+    this.findById(this.idVehicle);
   }
 
   onSubmit() {
+    const fnAction = this.idVehicle ? () => this.updateVehicle() : () => this.saveVehicle();
+    const action: string = this.idVehicle ? 'atualizar' : 'cadastrar';
+
     this.dialog.open(DialogComponent, {
       width: '350px',
       height: '300px',
-      data: {fnAction: () => this.saveVehicle(), title: `Deseja salvar o veículo "${this.vehicle.placa}" ?`, subtitle: 'Esta ação não poderá ser desfeita !'}
+      data: {fnAction, title: `Deseja ${action} o veículo "${this.vehicle.placa}" ?`, subtitle: 'Esta ação não poderá ser desfeita !'}
     });
   }
 
+  async findById(vehicleId: string) {
+    (await this.vehicleService.findById(vehicleId))
+    .subscribe((vehicle: Vehicle) => {
+      this.vehicle = new Vehicle(vehicle.id, vehicle.status, vehicle.placa, vehicle.cor, vehicle.marca, vehicle.modelo, vehicle.valor, vehicle.kmAtual, vehicle.anoFabricacao, vehicle.anoModelo);
+    }, err => alert(err));
+  }
+
   async saveVehicle() {
-      //Salva o objeto no localStorage
+      // Salva o objeto no localStorage
       // localStorage.setItem('vehicle', JSON.stringify(this.vehicle));
 
-      //Salva o objeto no Json-Server
+      // Salva o objeto no Json-Server
       this.vehicleService.save(this.vehicle)
       .then(() => this.location.back())
       .catch((err) => alert(err));
+  }
+
+  // Atualiza o veículo
+  async updateVehicle() {
+    this.vehicleService.update(this.vehicle)
+    .then(() => this.location.back())
+    .catch((err) => alert(err));
   }
 
   handleCancel() {
--- src/app/services/vehicle.service.ts
+++ src/app/services/vehicle.service.ts
@@ -1,5 +1,6 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable } from 'rxjs';
 import { Vehicle } from '../model/Vehicle';
 
 @Injectable({
@@ -21,11 +22,26 @@ export class VehicleService {
     });
   }
 
+  async update(vehicle: Vehicle) {
+    return this.httpClient.put(`${this.URI}/${vehicle.id}`, JSON.stringify(vehicle), this.httpOptions).toPromise()
+    .then(response => response as Vehicle)
+    .catch(() => {
+      throw new Error('Erro ao atualizar o veículo !');
+    });
+  }
+
   async findAll() {
     return this.httpClient.get(this.URI).toPromise()
     .then(response => response as Vehicle[])
     .catch(() => {
       throw new Error('Erro ao buscar o veículo !');
     });
+  }
+
+  async findById(vehicleId: string):Promise<Observable<Vehicle>> {
+    return this.httpClient.get<Vehicle>(`${this.URI}/${vehicleId}`)
+    .pipe(catchError((err) => {
+      throw new Error(err)
+    }));
   }
 }
